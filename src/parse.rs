0276eb658ba5075a69933405cb295cbac004f58e
rjhallsted <rjhallsted@gmail.com>
2024-07-10 21:08:23 -0600

    Refactor to take better advantage of the api quickxml provides

--- src/parse.rs
+++ src/parse.rs
@@ -1,5 +1,8 @@
 use chrono::NaiveDateTime;
-use quick_xml::{events::Event, Error, Reader};
+use quick_xml::{
+    events::{BytesEnd, BytesStart, Event},
+    Error, Reader,
+};
 
 use crate::Post;
 
@@ -11,24 +14,26 @@ enum Tag {
     PubDate,
     None,
 }
+impl Tag {
+    fn name(&self) -> Option<&[u8]> {
+        match self {
+            Tag::Item => Some(b"item"),
+            Tag::Title => Some(b"title"),
+            Tag::Link => Some(b"link"),
+            Tag::PubDate => Some(b"pubDate"),
+            Tag::None => None,
+        }
+    }
 
-// TODO: Add some tests proving this state machine actually catches malformed xml the way
-// we expect
-
-/// Represents the parse states we want to verify
-///
-/// The allowed transitions are:
-/// - OutOfItem ->
-///     
-///   loops: InItemOutOfTag -> InItemTagOpened -> InItemTextConsumed
-///
-///   -> OutOfItem
-#[derive(PartialEq)]
-enum ParseState {
-    OutOfItem,
-    InItemOutOfTag,
-    InItemTagOpened,
-    InItemTextConsumed,
+    fn from_name(name: &[u8]) -> Self {
+        match name {
+            b"item" => Tag::Item,
+            b"title" => Tag::Title,
+            b"link" => Tag::Link,
+            b"pubDate" => Tag::PubDate,
+            _ => Tag::None,
+        }
+    }
 }
 
 /// NOTE: This currently assumes (mostly) that the xml is
@@ -36,147 +41,55 @@ enum ParseState {
 pub struct Parser<'a, 'b> {
     reader: Reader<&'a [u8]>,
     author: &'b str,
-    state: ParseState,
     done: bool,
 }
 impl<'a, 'b> Parser<'a, 'b> {
     pub fn new(input: &'a str, author: &'b str) -> Self {
-        Parser {
-            reader: Reader::from_str(&input),
+        let mut reader = Reader::from_str(&input);
+        reader.config_mut().trim_text(true);
+        let parser = Parser {
+            reader: reader,
             author: &author,
-            state: ParseState::OutOfItem,
             done: false,
-        }
+        };
+
+        parser
     }
 
-    /// Find the next opening tag type, of the types we recognize
-    /// in `Tag`
-    fn find_open_tag(&mut self) -> Result<Option<Tag>, Error> {
-        assert!(matches!(
-            self.state,
-            ParseState::OutOfItem | ParseState::InItemOutOfTag
-        ));
-        let mut tag = Tag::None;
-        while matches!(tag, Tag::None) {
-            let event = match self.reader.read_event() {
-                Ok(event) => event,
-                Err(err) => {
-                    return Err(err);
-                }
-            };
-            tag = match event {
-                Event::Start(t) => match t.name().as_ref() {
-                    b"item" => Tag::Item,
-                    b"title" => Tag::Title,
-                    b"link" => Tag::Link,
-                    b"pubDate" => Tag::PubDate,
-                    _ => Tag::None,
-                },
-                Event::Eof => {
-                    self.done = true;
-                    return Ok(None);
-                }
-                _ => Tag::None,
-            };
-        }
-        match tag {
-            Tag::Item => {
-                assert!(matches!(self.state, ParseState::OutOfItem));
-                self.state = ParseState::InItemOutOfTag;
-            }
-            _ => {
-                // The title and link tags can appear outside of items,
-                // so we can't assert the prior state when they're opened,
-                // so instead we only transition state when the desired prior
-                // state is present.If the document is malformed, our other
-                // state checks should catch it.
-                if matches!(self.state, ParseState::InItemOutOfTag) {
-                    self.state = ParseState::InItemTagOpened;
-                }
-            }
-        }
-        Ok(Some(tag))
-    }
+    fn read_through_start(&mut self, tag: Tag) -> Result<Option<BytesStart>, Error> {
+        assert!(!matches!(tag, Tag::None));
 
-    /// Find the next piece of text to consume
-    fn find_text(&mut self) -> Result<Option<String>, Error> {
-        assert!(matches!(self.state, ParseState::InItemTagOpened));
-        loop {
-            let event = match self.reader.read_event() {
-                Ok(event) => event,
-                Err(err) => {
-                    return Err(err);
-                }
-            };
+        while let Ok(event) = self.reader.read_event() {
             match event {
-                Event::Text(text) => {
-                    self.state = ParseState::InItemTextConsumed;
-                    return text.unescape().map(|t| String::from(t)).map(|t| Some(t));
-                }
+                Event::Start(t) if t.name().as_ref() == tag.name().unwrap() => return Ok(Some(t)),
                 Event::Eof => {
                     self.done = true;
-                    return Ok(None);
+                    break;
                 }
                 _ => (),
             }
         }
+        Ok(None)
     }
 
     /// Consumes through the next closing tag of of type `tag`
-    fn consume_close_tag(&mut self, tag: &Tag) -> Result<(), Error> {
-        assert!(matches!(
-            self.state,
-            ParseState::InItemTextConsumed | ParseState::InItemOutOfTag
-        ));
-        loop {
-            let event = match self.reader.read_event() {
-                Ok(event) => event,
-                Err(err) => {
-                    return Err(err);
-                }
-            };
-            match event {
-                Event::End(t) => match (t.name().as_ref(), tag) {
-                    (b"item", Tag::Item) => {
-                        assert!(matches!(self.state, ParseState::InItemOutOfTag));
-                        self.state = ParseState::OutOfItem;
-                        return Ok(());
-                    }
-                    (b"title", Tag::Title) | (b"link", Tag::Link) | (b"pubDate", Tag::PubDate) => {
-                        assert!(matches!(self.state, ParseState::InItemTextConsumed));
-                        self.state = ParseState::InItemOutOfTag;
-                        return Ok(());
-                    }
-                    _ => (),
-                },
-                Event::Eof => {
-                    self.done = true;
-                    return Ok(());
-                }
-                _ => (),
-            };
-        }
+    fn consume_close_tag(&mut self, end: BytesEnd) -> Result<(), Error> {
+        self.reader.read_to_end(end.name())?;
+        Ok(())
     }
 
     /// Returns the next tag type and its contents. Assumes you are _in_ an `<item>`
     fn consume_next_tag(&mut self) -> Result<Option<(Tag, String)>, Error> {
-        let tag = self.find_open_tag()?;
-        let tag = match tag {
-            Some(tag) => tag,
-            None => return Ok(None),
+        let next_event = self.reader.read_event()?;
+        let start = match next_event {
+            Event::Start(t) => t,
+            Event::Eof => return Ok(None),
+            _ => panic!("Should be impossible. XML is likely malformed."),
         };
-        assert!(matches!(self.state, ParseState::InItemTagOpened));
+        let end = start.to_end();
+        let tag = Tag::from_name(start.name().as_ref());
 
-        let text = self.find_text()?;
-        let text = match text {
-            Some(text) => text,
-            None => return Ok(None),
-        };
-        assert!(matches!(self.state, ParseState::InItemTextConsumed));
-
-        self.consume_close_tag(&tag)?;
-        assert!(matches!(self.state, ParseState::InItemOutOfTag));
-
+        let text = self.reader.read_text(end.name())?.into_owned();
         Ok(Some((tag, text)))
     }
 
@@ -192,17 +105,10 @@ impl<'a, 'b> Parser<'a, 'b> {
         }
 
         // find item opening tag
-        assert!(matches!(self.state, ParseState::OutOfItem));
-        loop {
-            let tag = match self.find_open_tag()? {
-                Some(t) => t,
-                None => return Ok(None),
-            };
-            if matches!(tag, Tag::Item) {
-                break;
-            }
-        }
-        assert!(matches!(self.state, ParseState::InItemOutOfTag));
+        let start = match self.read_through_start(Tag::Item)? {
+            Some(s) => s.to_owned(),
+            None => return Ok(None),
+        };
 
         // get post parts
         let mut link: Option<String> = None;
@@ -222,17 +128,15 @@ impl<'a, 'b> Parser<'a, 'b> {
                         .map(|x| Some(x))
                         .expect("Date parsing failed")
                 }
-                Tag::None => panic!("Shouldn't happen"), //TODO: remove panic via wrapped error
+                Tag::None => (),
             }
         }
         let link = link.take().expect("There should be an link here");
         let title = title.take().expect("There should be an title here");
         let date = date.take().expect("There should be an date here");
-        assert!(matches!(self.state, ParseState::InItemOutOfTag));
 
         // consume the closing tag
-        self.consume_close_tag(&Tag::Item)?;
-        assert!(matches!(self.state, ParseState::OutOfItem));
+        self.consume_close_tag(start.to_end())?;
 
         Ok(Some(Post {
             link,
